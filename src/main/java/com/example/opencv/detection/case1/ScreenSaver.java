4320f5de499621612c8c29645aa12ea79745b391
baby7305 <songhaibei7305@163.com>
2019-09-05 15:45:33 +0800

    抓捕荧幕画面

---
+++ src/main/java/com/example/opencv/detection/case1/ScreenSaver.java
@@ -0,0 +1,81 @@
+package com.example.opencv.detection.case1;
+
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class ScreenSaver {
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+    }
+
+    static void save(String path) throws Exception {
+        Robot robot = new Robot();
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle rect = new Rectangle(0, 0, d.width, d.height);
+        BufferedImage image = robot.createScreenCapture(rect);
+        ImageIO.write(image, "jpg", new File(path));
+
+    }
+//		public static void main(String[] args) {
+//		try{
+//		Ch10_25_1ScreenSaver.save("C://opencv3.1//samples/MyScreen.png");
+//		}catch(Exception e){
+//		e.printStackTrace();
+//		}
+//		}
+
+    public static void main(String[] args) throws AWTException {
+        Robot robot = new Robot();
+        robot.delay(5000);
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+        Rectangle rect = new Rectangle(0, 0, d.width, d.height);
+        BufferedImage image = robot.createScreenCapture(rect);
+        //BufferedImage im = new BufferedImage(image.getWidth(), image.getHeight(),BufferedImage.TYPE_3BYTE_BGR);
+        System.out.println("image.getType()=" + image.getType());
+        System.out.println("BufferedImage.TYPE_BYTE_GRAY=" + BufferedImage.TYPE_BYTE_GRAY);
+        System.out.println("BufferedImage.TYPE_4BYTE_ABGR=" + BufferedImage.TYPE_4BYTE_ABGR);
+        System.out.println("BufferedImage.TYPE_INT_BGR=" + BufferedImage.TYPE_INT_BGR);
+        System.out.println("BufferedImage.TYPE_INT_RGB=" + BufferedImage.TYPE_INT_RGB);
+        Mat result = bufferedImg2Mat(image);
+
+        Imgcodecs.imwrite("C:\\opencv-data//MyScreen.png", result);
+    }
+
+    public static Mat bufferedImg2Mat(BufferedImage in) {
+        Mat out;
+        byte[] data;
+        int r, g, b;
+
+        if (in.getType() == BufferedImage.TYPE_INT_RGB) {
+            out = new Mat(in.getHeight(), in.getWidth(), CvType.CV_8UC3);
+            data = new byte[in.getHeight() * in.getWidth() * (int) out.elemSize()];
+            int[] dataBuff = in.getRGB(0, 0, in.getWidth(), in.getHeight(), null, 0, in.getWidth());
+            for (int i = 0; i < dataBuff.length; i++) {
+                data[i * 3] = (byte) ((dataBuff[i] >> 0) & 0xFF);
+                data[i * 3 + 1] = (byte) ((dataBuff[i] >> 8) & 0xFF);
+                data[i * 3 + 2] = (byte) ((dataBuff[i] >> 16) & 0xFF);
+            }
+        } else {
+            out = new Mat(in.getHeight(), in.getWidth(), CvType.CV_8UC1);
+            data = new byte[in.getHeight() * in.getWidth() * (int) out.elemSize()];
+            int[] dataBuff = in.getRGB(0, 0, in.getWidth(), in.getHeight(), null, 0, in.getWidth());
+            for (int i = 0; i < dataBuff.length; i++) {
+                r = (byte) ((dataBuff[i] >> 16) & 0xFF);
+                g = (byte) ((dataBuff[i] >> 8) & 0xFF);
+                b = (byte) ((dataBuff[i] >> 0) & 0xFF);
+                data[i] = (byte) ((0.21 * r) + (0.71 * g) + (0.07 * b)); // luminosity
+            }
+        }
+        out.put(0, 0, data);
+        return out;
+    }
+
+
+}
