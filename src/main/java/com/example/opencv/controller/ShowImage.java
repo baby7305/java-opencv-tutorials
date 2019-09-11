0f447bc4586a7af316848e5617f18fd2c807fb1e
baby7305 <songhaibei7305@163.com>
2019-09-11 17:18:05 +0800

    springboot返回opencv图片

--- src/main/java/com/example/opencv/controller/ShowImage.java
+++ src/main/java/com/example/opencv/controller/ShowImage.java
@@ -1,5 +1,9 @@
 package com.example.opencv.controller;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,5 +26,19 @@ public class ShowImage {
         byte[] bytes = new byte[inputStream.available()];
         inputStream.read(bytes, 0, inputStream.available());
         return bytes;
+    }
+
+    @GetMapping(value = "/opencv", produces = MediaType.IMAGE_JPEG_VALUE)
+    @ResponseBody
+    public byte[] getImage1() throws IOException {
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        //System.load(arg0);
+
+        Mat source = Imgcodecs.imread("C:\\opencv-data//lena.jpg");
+        MatOfByte buffer = new MatOfByte();
+        Imgcodecs.imencode(".jpg", source, buffer);//對影像編碼並且放置於緩衝記憶體
+        byte data1[] = buffer.toArray();//轉陣列
+        return data1;
     }
 }
